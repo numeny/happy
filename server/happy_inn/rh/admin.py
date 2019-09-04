c524c5e32d9823898032aeed754d8d56b22593f8
baidaogui <baidaogui@sogou-inc.com>
2019-09-04 12:41:56 +0800

    Display tables on django's admin
    N/A
    
    .
    
    Change-Id: Ieb2fe47b901496d4d7c8c051d65211f6ced88569

--- server/happy_inn/rh/admin.py
+++ server/happy_inn/rh/admin.py
@@ -3,4 +3,33 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from rh.models import rh
+from rh.models import city
+from rh.models import favorite
+from rh.models import RhUser
+
+
+
 # Register your models here.
+class rhAdmin(admin.ModelAdmin):
+    list_display = ('id','rh_name', 'rh_type')
+    search_fields = ('id', )
+
+class cityAdmin(admin.ModelAdmin):
+    list_display = ('privince', 'city', 'area')
+    search_fields = ('city', )
+
+class favoriteAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'rhId')
+    search_fields = ('uid', )
+
+class RhUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'user_type', 'unionid', 'openid')
+    search_fields = ('username', )
+
+
+# Register your models here.
+admin.site.register(rh, rhAdmin)
+admin.site.register(favorite, favoriteAdmin)
+admin.site.register(RhUser, RhUserAdmin)
+admin.site.register(city, cityAdmin)
