d4be4aa0814470aec39e40af15e0f482a9d1bbdc
baidaogui <baidaogui@sogou-inc.com>
2019-09-04 12:02:05 +0800

    Add f.js on envsetup.sh
    N/A
    
    .
    
    Change-Id: I26b4167e02faa40f7122a124ec58eebb4aaa0148

--- envsetup.sh
+++ envsetup.sh
@@ -92,3 +92,20 @@ function runserver() {
   cd $HAPPY_SERVER_PATH
   python manage.py runserver 0.0.0.0:8001&
 }
+
+function f.js() {
+  if [ $# -eq 1 ];then
+      SEARCH_PATH=.
+      SEARCH_STR="$1"
+  elif [ $# -eq 2 ];then
+      SEARCH_PATH=$1
+      SEARCH_STR="$2"
+  else
+      echo "[Usage] $0 [search_path] search_string"
+      exit 0
+  fi
+
+  echo "$0 $SEARCH_PATH $SEARCH_STR"
+
+  find $SEARCH_PATH -regex '.*\.\(js\|tsx\)' -type f | xargs grep "$SEARCH_STR" --color=auto -i
+}
