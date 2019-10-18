fef9f3153731a3965ec10658b80cfd975f493caf
baidaogui <baidaogui@sogou-inc.com>
2019-10-18 12:01:22 +0800

    Revert "iHome : calcDeepTaxRate -> calcDeedTaxRate - 2"
    This reverts commit b20d47f4dd50e7602c830a928041f035a234cd38.

--- ihome/src/redux/actions/deedTaxRate.js
+++ ihome/src/redux/actions/deedTaxRate.js
@@ -3,10 +3,12 @@ import {
 } from '../constants/deedTaxRate'
 
 
-export const calcDeedTaxRate = (isFirstHouse, houseArea) => {
+export const calcDeepTaxRate = (isFirstHouse, houseArea) => {
+// export const calcDeepTaxRate = () => {
   return {
     type: CALC_DEEP_TAX_RATE,
     isFirstHouse: isFirstHouse,
     houseArea: houseArea
+    // deedTaxRate: 0.8,
   }
 }
